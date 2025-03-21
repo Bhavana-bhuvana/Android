f4cdced2aa2cd8fe7e487030e00e5f72222961d3
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-03-21 15:27:05 +0530

    Update MainActivity.java

--- program7/MainActivity.java
+++ program7/MainActivity.java
@@ -5,12 +5,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
